16e32f268e2cb6f595569949ecf774ec16fe4232
su-kun1899 <higedrum.coz@gmail.com>
2016-07-13 21:13:58 +0900

    平均値のテストケースを追加

--- test/iloveyouboss/ScoreCollectionTest.java
+++ test/iloveyouboss/ScoreCollectionTest.java
@@ -3,12 +3,22 @@ package iloveyouboss;
 
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 public class ScoreCollectionTest {
   @Test
-  public void test() throws Exception {
-    fail("まだ実装されていません");
+  public void answersArithmeticMeanOfTwoNumbers() throws Exception {
+    // Arrange
+    ScoreCollection collection = new ScoreCollection();
+    collection.add(() -> 5);
+    collection.add(() -> 7);
+
+    // Act
+    int actualResult = collection.arithmeticMean();
+
+    // Assert
+    assertThat(actualResult, equalTo(6));
   }
 
 }
